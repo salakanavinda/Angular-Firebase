b015e15a4984e61683a3a9ed36c69aab3716b995
Salaka Navinda <mmsnavinda@gmail.com>
2020-04-15 16:07:52 +0530

    Initial setup functions

--- Application/AngularWithFireBase/src/app/components/orders/orders.component.ts
+++ Application/AngularWithFireBase/src/app/components/orders/orders.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { OrdersService } from 'src/app/services/orders.service';
 import { FormGroup, FormControl } from '@angular/forms';
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: 'app-orders',
@@ -8,17 +9,38 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./orders.component.css']
 })
 export class OrdersComponent implements OnInit {
-   
-  coffees = ["Americano", "Flat White", "Cappuccino", "Latte", "Espresso", "Machiato", "Mocha", "Hot Chocolate", "Tea"];
-  form = new FormGroup({        
-    customerName: new FormControl(''),
-    orderNumber: new FormControl(''),
-    coffeeOrder: new FormControl(''), 
-    completed: new FormControl(false)
-})
+  
   constructor(private ordersService:OrdersService) { }
 
   ngOnInit(): void {
   }
+
+  form = new FormGroup({
+    customerName: new FormControl(''),
+    orderNumber: new FormControl(''),
+    coffeeOrder: new FormControl(''), 
+    completed: new FormControl(false)
+  });
   
+  coffees = ["Americano", "Flat White", "Cappuccino", "Latte", "Espresso", "Machiato", "Mocha", "Hot Chocolate", "Tea"];
+
+  coffeeOrder = [];
+
+  addCoffee = coffee => this.coffeeOrder.push(coffee);
+
+  removeCoffee = coffee => {
+    let index = this.coffeeOrder.indexOf(coffee);
+    if (index > -1) this.coffeeOrder.splice(index, 1);
+  };
+
+  onSubmit() {
+    this.form.value.coffeeOrder = this.coffeeOrder;
+    let data = this.form.value;
+    console.log(data);
+
+    // this.ordersService.createCoffeeOrder(data).then(res => {
+    //   /*do something here....maybe clear the form or give a success message*/
+    // });
+  }
+
 }
--- Application/AngularWithFireBase/src/app/services/orders.service.ts
+++ Application/AngularWithFireBase/src/app/services/orders.service.ts
@@ -8,4 +8,21 @@ import { FormControl, FormGroup } from "@angular/forms";
 export class OrdersService {
 
   constructor() { }
+
+  form = new FormGroup({
+    customerName: new FormControl(''),
+    orderNumber: new FormControl(''),
+    coffeeOrder: new FormControl(''), 
+    completed: new FormControl(false)
+  });
+
+
+  createCoffeeOrder(data) {
+    // return new Promise<any>((resolve, reject) => {
+    //   this.firestore
+    //     .collection("coffeeOrders")
+    //     .add(data)
+    //     .then(res => {}, err => reject(err));
+    // });
+  }
 }
